0c21f0a610eb8b875d40c2c9cdaa82eca0ea8345
Amirtaha Javadi <amirtahajavadidev@gmail.com>
2025-07-17 15:14:42 +0330

    database functionality added

---
+++ score_bot/couch.py
@@ -0,0 +1,31 @@
+import couchdb
+from datetime import datetime
+
+class couchDatabase:
+    def __init__(self , url ,db_name):
+        couch = couchdb.Server(url)
+        if db_name in couch:
+            db = couch[db_name]
+        else:
+            db = couch.create(db_name)
+        self.db = db
+    def addPoints(self , user_id , points):
+        getuser = self.db.get("user:"+user_id)
+        if getuser:
+            print("User exists")
+            getuser["points"] += points
+            self.db.save(getuser)
+            print("Points added")
+
+        else:
+            user_doc = {
+            "_id": f"user:{user_id}",
+            "type": "user",
+            "telegram_id": user_id,
+            "points": points,
+            "last_active": datetime.now().isoformat()}
+            self.db.save(user_doc)
+            print("User does not exist")
+
+couchdb = couchDatabase("http://admin:password@host:port/" , "users")
+couchdb.addPoints("12345634" , 100)
